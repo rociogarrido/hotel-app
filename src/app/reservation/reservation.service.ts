461f8b0033b2b32fa9aaf0f2d69068278199a9cb
Rocio Garrido <rocioganon@gmail.com>
2024-11-05 09:35:49 +0100

    feat: inject the service into the form component

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -19,6 +19,8 @@ export class ReservationService {
 
   addReservation(reservation: Reservation): void {
     this.reservations.push(reservation);
+    console.log(this.reservations);
+    
   }
 
   deleteReservation(id: string): void {

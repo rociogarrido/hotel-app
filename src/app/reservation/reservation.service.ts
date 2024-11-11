d7898baa895ef44d1f48d76c16e458a1388e3eb1
Rocio Garrido <rocioganon@gmail.com>
2024-11-11 13:28:43 +0100

    feat: prepare the project for the mock backend api

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -7,11 +7,6 @@ import { Reservation } from '../models/reservation';
 export class ReservationService {
   private reservations: Reservation[] = [];
 
-  constructor() {
-    let savedReservations = localStorage.getItem('reservations');
-    this.reservations = savedReservations ? JSON.parse(savedReservations) : [];
-  }
-
   // CRUD
 
   getReservations(): Reservation[] {
@@ -26,20 +21,15 @@ export class ReservationService {
     reservation.id = Date.now().toString();
 
     this.reservations.push(reservation);
-    localStorage.setItem('reservations', JSON.stringify(this.reservations));
   }
 
   deleteReservation(id: string): void {
     let index = this.reservations.findIndex((res) => res.id === id);
     this.reservations.splice(index, 1);
-    localStorage.setItem('reservations', JSON.stringify(this.reservations));
   }
 
   updateReservation(id: string, updatedReservation: Reservation): void {
-    let index = this.reservations.findIndex(
-      (res) => res.id === id
-    );
+    let index = this.reservations.findIndex((res) => res.id === id);
     this.reservations[index] = updatedReservation;
-    localStorage.setItem('reservations', JSON.stringify(this.reservations));
   }
 }
